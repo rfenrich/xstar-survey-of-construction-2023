50ed4a315abeccdeb0130c89a734b2f2e027b8c3
rfenrich <rwfenrich@gmail.com>
2025-01-14 15:26:31 -0500

    add data and simple sf vs. cost plot

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
+st.title("Survey of Construction 2023")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Investigation of the 2023 US Census Bureau Survey of Construction."
 )
+
+data = pd.read_csv("soc2023.csv")
+
+st.scatter_chart(data, x="FSQFS", x_label="Square Foot Area", y="SLPR", y_label="Sales Price")
