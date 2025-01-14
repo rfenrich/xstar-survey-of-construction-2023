75756aa2d974d1b417ce3d3f3f6a73df482bab44
rfenrich <rwfenrich@gmail.com>
2025-01-14 16:09:56 -0500

    allow user to choose x and y axes and ignore missing data

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,21 @@ st.write(
 )
 
 data = pd.read_csv("soc2023.csv")
+vars = pd.read_csv("soc_variables.csv")
 
-st.scatter_chart(data, x="FSQFS", x_label="Square Foot Area", y="SLPR", y_label="Sales Price")
+data_options = sorted(vars["Description"].to_list())
+
+x_label = st.selectbox("X axis", options=data_options, index=data_options.index("Square Foot Area of House"))
+x_key = vars.loc[vars["Description"] == x_label, "Variable"].values[0]
+st.text(vars.loc[vars["Description"] == x_label, "Possible Values"].values[0])
+
+y_label = st.selectbox("Y axis", options=data_options, index=data_options.index("Final Sales Price at Completion"))
+y_key = vars.loc[vars["Description"] == y_label, "Variable"].values[0]
+st.text(vars.loc[vars["Description"] == y_label, "Possible Values"].values[0])
+
+
+drop_na = st.checkbox("Drop missing values", value=True)
+if drop_na:
+    data = data.drop(data[(data[x_key] <= 0) | (data[y_key] <= 0)].index)
+
+st.scatter_chart(data, x=x_key, x_label=x_label, y=y_key, y_label=y_label)
